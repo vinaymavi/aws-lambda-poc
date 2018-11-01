4f90ad702dfb15e311becebee8e83ac1f5247fe2
vinay mavi <vinaymavi@gmail.com>
2018-11-01 12:45:15 +0530

    Lambda function response updated.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.handler = async(event,context) => {
-    return "Hello World, waiting for code deploy server to build!!! 26/Oct Morning..."
+exports.handler = (event,context,callback) => {
+    var responseBody = {
+        message: 'Hello World, waiting for code deploy server to build!!! 26/Oct Morning...'
+    }
+    var response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify(responseBody)
+    };
+    console.log("response: " + JSON.stringify(response))
+    callback(null, response);
 }
