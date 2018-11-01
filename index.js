5a8a3683732707f21d556f6abbe325ed3b2a44a1
vinay mavi <vinaymavi@gmail.com>
2018-11-01 17:35:09 +0530

    path configured.

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-exports.handler = (event,context,callback) => {
-    console.log("event=",JSON.stringify(event));
-    var responseBody = {
-        message: 'Hello World, waiting for code deploy server to build!!! 26/Oct Morning...'
-    }
-    var response = {
-        statusCode: 200,
-        headers: {
-            "x-custom-header" : "my custom header value"
-        },
-        body: JSON.stringify(responseBody)
-    };
-    console.log("response: " + JSON.stringify(response))
-    callback(null, response);
+exports.handler = async (event, context) => {
+  console.log("event=", JSON.stringify(event));
+
+  let response = {
+    statusCode: 200,
+    headers: {
+      "x-custom-header": "my custom header value"
+    },
+    body: ""
+  };
+  let respBody = "";
+  const path = event && event.pathParameters && event.pathParameters.proxy;
+  switch (path) {
+    case "getOne":
+      respBody = getOne();
+      break;
+    case "createOne":
+      respBody = createOne();
+      break;
+    default:
+      respBody = { whoami: "I don't know" };
+  }
+  response.body = JSON.stringify(respBody);
+  return response;
+};
+
+function getOne() {
+  return {
+    whoami: "getOne"
+  };
+}
+
+function createOne() {
+  return {
+    whoami: "createOne"
+  };
 }
